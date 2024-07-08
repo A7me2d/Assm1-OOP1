82d5998e89302cdaf14889466fe9536e3dae9fba
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-08 22:30:45 +0300

    Q1 Solve

---
+++ Assment1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assment1
+{
+
+    public enum WeekDays
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 : Create an enum called "WeekDays" with the days of the week
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays))) 
+            //{ 
+            //Console.WriteLine(day);
+            //}
+            #endregion
+        }
+
+       
+
+    }
+}
