0c99524bc28d3b4d3f73b645b46cf60acb599aa9
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-08 19:10:33 +0300

    DoSomeProtectCode

--- Assm1 OOP1/Program.cs
+++ Assm1 OOP1/Program.cs
@@ -2,9 +2,66 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void DoSomeCode()
+        {
+            try
+            {
+                int x, y, z;
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+                z = x / y;
+
+                int[] Numbers = { 1, 2, 3 };
+                Numbers[10] = 100;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void DoSomeProtectCode()
         {
 
+            int x, y, z;
+            bool Flag;
+            do
+            {
+                Console.WriteLine("Enter Firt Num");
+                Flag = int.TryParse(Console.ReadLine(), out x);
+            } while (!Flag);
+
+            do 
+            {
+                Console.WriteLine("Enter Firt Num");
+                Flag = int.TryParse(Console.ReadLine(), out y);
+            } while (!Flag || y == 0);
+            //x = int.Parse(Console.ReadLine());
+            //int.TryParse(Console.ReadLine(), out x);
+            //y = int.Parse(Console.ReadLine());
+            z = x / y;
+
+            int[] Numbers = { 1, 2, 3 };
+            if(Numbers.Length > 10 )
+            {
+                Numbers[10] = 100;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoSomeProtectCode();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finaly");
+            }
+            //DoSomeCode();
         }
     }
 }
