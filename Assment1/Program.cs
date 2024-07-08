d2e5631efb61e6692c4df52063ea0f8e083f6a2d
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-08 22:36:01 +0300

    Q3 solve

--- Assment1/Program.cs
+++ Assment1/Program.cs
@@ -19,6 +19,16 @@
         Autumn,
         Winter
     }
+
+    [Flags]
+    public enum Permissions
+    {
+        None = 0,
+        Read = 1,
+        Write = 2,
+        Delete = 4,
+        Execute = 8
+    }
     class Program
     {
         static void Main(string[] args)
@@ -31,34 +41,68 @@
             #endregion
 
             #region Q2 : Create an enum called "Season" 
-            Console.WriteLine("Enter a season name (Spring, Summer, Autumn, Winter):");
-            string input = Console.ReadLine();
-            if (Enum.TryParse(input, true, out Season season))
-            {
-                switch (season)
-                {
-                    case Season.Spring:
-                        Console.WriteLine("Spring: March to May");
-                        break;
-                    case Season.Summer:
-                        Console.WriteLine("Summer: June to August");
-                        break;
-                    case Season.Autumn:
-                        Console.WriteLine("Autumn: September to November");
-                        break;
-                    case Season.Winter:
-                        Console.WriteLine("Winter: December to February");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid season");
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid season name.");
-            }
+            //Console.WriteLine("Enter a season name (Spring, Summer, Autumn, Winter):");
+            //string input = Console.ReadLine();
+            //if (Enum.TryParse(input, true, out Season season))
+            //{
+            //    switch (season)
+            //    {
+            //        case Season.Spring:
+            //            Console.WriteLine("Spring: March to May");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("Summer: June to August");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("Autumn: September to November");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("Winter: December to February");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid season");
+            //            break;
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid season name.");
+            //}
             #endregion
+
+            #region Q3 : Assign the following Permissions (Read, write, Delete, Execute)
+            //Permissions userPermissions = Permissions.None;
+
+            //userPermissions |= Permissions.Read;
+            //userPermissions |= Permissions.Write;
+
+            //Console.WriteLine("Current Permissions: " + userPermissions);
+
+            //if ((userPermissions & Permissions.Read) == Permissions.Read)
+            //{
+            //    Console.WriteLine("Read permission is granted.");
+            //}
+
+            //if ((userPermissions & Permissions.Delete) == Permissions.Delete)
+            //{
+            //    Console.WriteLine("Delete permission is granted.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Delete permission is not granted.");
+            //}
+
+            //userPermissions &= ~Permissions.Write;
+
+            //Console.WriteLine("Permissions after removal: " + userPermissions);
+
+            //if ((userPermissions & Permissions.Write) != Permissions.Write)
+            //{
+            //    Console.WriteLine("Write permission is removed.");
+            //}
+            #endregion
+
+
         }
 
 
