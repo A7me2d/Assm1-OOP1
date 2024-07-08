ca5de777f31e7c45ea8c388fb6f6f8d86c46d82a
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-08 22:32:51 +0300

    Q2 solve

--- Assment1/Program.cs
+++ Assment1/Program.cs
@@ -11,6 +11,14 @@
         Thursday,
         Friday
     }
+
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
     class Program
     {
         static void Main(string[] args)
@@ -21,9 +29,39 @@
             //Console.WriteLine(day);
             //}
             #endregion
+
+            #region Q2 : Create an enum called "Season" 
+            Console.WriteLine("Enter a season name (Spring, Summer, Autumn, Winter):");
+            string input = Console.ReadLine();
+            if (Enum.TryParse(input, true, out Season season))
+            {
+                switch (season)
+                {
+                    case Season.Spring:
+                        Console.WriteLine("Spring: March to May");
+                        break;
+                    case Season.Summer:
+                        Console.WriteLine("Summer: June to August");
+                        break;
+                    case Season.Autumn:
+                        Console.WriteLine("Autumn: September to November");
+                        break;
+                    case Season.Winter:
+                        Console.WriteLine("Winter: December to February");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid season");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid season name.");
+            }
+            #endregion
         }
 
-       
+
 
     }
 }
